07a2b3fee7e72c945b3e9799fa647ff1b2898c21
ejwood003 <ejwood@byu.edu>
2026-03-23 09:25:45 -0600

    Fixed bootstrap errors

--- backend/Mission11_Wood/Mission11_Wood/obj/Debug/net10.0/Mission11_Wood.AssemblyInfo.cs
+++ backend/Mission11_Wood/Mission11_Wood/obj/Debug/net10.0/Mission11_Wood.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Wood")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f290a65ce8b7536187224846b2e03e584d8107d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+104da24a127792e9e4a9e26b81d06d850f4b1df6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Wood")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Wood")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
